25579e6967c38be088d7ecdb542034a63b5833ee
SANDEEP KUMAR JAISAWAL <s.jaisawal@samsung.com>
2023-11-09 23:33:21 +0530

    Create Model implemented end to end
    Issue-Id: AIMLFW-64
    
    Change-Id: I633ede09e7ae6b903f10bb85580071a213fb99da
    Signed-off-by: SANDEEP KUMAR JAISAWAL <s.jaisawal@samsung.com>

--- core/s3_manager.go
+++ core/s3_manager.go
@@ -1,26 +1,66 @@
 package core
 
 import (
+	"bytes"
 	"fmt"
 	"io"
 	"os"
 
 	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/credentials"
+	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/s3"
 )
 
 type S3_manager struct {
-	//Have aws properties, so that they are accessible
-	//to all the function of this file
+	//S3Client has s3 endpoint connection pointer,
+	//Which will be used by all s3 bucket related operatios,
+	//using fuction to struct binding
+	S3Client *s3.S3
 }
 
-func (s3manager *S3_manager) getBucketObject(client *s3.S3, objectName string, bucketName string) {
+/*
+This function return an pointer to instance of S3_manager struct
+the struct instance hold pointer to s3.S3 connection, which is
+preconfigured using enviroment variables, such as aws s3
+endpoints connection details.
+*/
+func New_s3manager() *S3_manager {
+	endpoint := os.Getenv("S3_URL")
+	accessKey := os.Getenv("S3_ACCESS_KEY")
+	SecretAccessKey := os.Getenv("S3_SECRETE_KEY")
+
+	config := aws.Config{
+		Endpoint:         aws.String(endpoint),
+		Credentials:      credentials.NewStaticCredentials(accessKey, SecretAccessKey, ""),
+		Region:           aws.String(os.Getenv("S3_REGION")),
+		S3ForcePathStyle: aws.Bool(true),
+	}
+	sess, err := session.NewSession(&config)
+
+	if err != nil {
+		panic(err)
+	}
+	S3Client := s3.New(sess)
+	return &S3_manager{S3Client} //Return new instance of S3_manager with all config loaded
+
+}
+
+func (s3manager *S3_manager) CreateBucket(bucketName string) {
+	_, err := s3manager.S3Client.CreateBucket(&s3.CreateBucketInput{Bucket: aws.String(bucketName)})
+	if err != nil {
+		panic(err)
+	}
+	println("Bucket created : ", bucketName)
+}
+
+func (s3manager *S3_manager) GetBucketObject(objectName string, bucketName string) {
 
 	getInputs := &s3.GetObjectInput{
 		Bucket: aws.String(bucketName),
 		Key:    aws.String(objectName),
 	}
-	result, err := client.GetObject(getInputs)
+	result, err := s3manager.S3Client.GetObject(getInputs)
 	defer result.Body.Close()
 
 	if err != nil {
@@ -28,12 +68,11 @@ func (s3manager *S3_manager) getBucketObject(client *s3.S3, objectName string, b
 		return
 	} else {
 		fmt.Println("Successfully retrieved object...")
-		fmt.Println(result)
 	}
 
-	outputFile, error := os.Create(bucketName + ".zip")
+	outputFile, error := os.Create(bucketName + ".json")
 	if error != nil {
-		fmt.Println("Could not temp local file ")
+		fmt.Println("Could not create local file ")
 		return
 	}
 	defer outputFile.Close()
@@ -44,7 +83,7 @@ func (s3manager *S3_manager) getBucketObject(client *s3.S3, objectName string, b
 	} else {
 		fmt.Println("Successfully, written bucket object to localfile")
 	}
-	//The file will be responded in getModel rest api
+
 }
 
 func (s3manager *S3_manager) deleteBucket(client *s3.S3, objectName string, bucketName string) {
@@ -75,25 +114,18 @@ func (s3manager *S3_manager) deleteBucketObject(client *s3.S3, objectName string
 	return true
 }
 
-func (s3manager *S3_manager) uploadFile(client *s3.S3, fileName string, bucketName string) {
-
-	file, err := os.Open(fileName)
-	if err != nil {
-		fmt.Println("Error is opening the file..")
-	}
-	defer file.Close()
+func (s3manager *S3_manager) UploadFile(data *bytes.Reader, file_name string, bucketName string) {
 
 	params := &s3.PutObjectInput{
 		Bucket: aws.String(bucketName),
-		Key:    aws.String(fileName),
-		Body:   file,
+		Key:    aws.String(file_name),
+		Body:   data,
 	}
-
-	_, err = client.PutObject(params)
+	_, err := s3manager.S3Client.PutObject(params)
 	if err != nil {
-		fmt.Printf("Error in uploading file to bucket ", err)
+		fmt.Println("Error in uploading file to bucket ", err)
 	}
-	fmt.Printf("File uploaded to bucket ")
+	fmt.Println("File uploaded to bucket ", bucketName)
 }
 
 func (s3manager *S3_manager) listBucket(client *s3.S3) {
@@ -108,12 +140,4 @@ func (s3manager *S3_manager) listBucket(client *s3.S3) {
 // Return list of objects in the buckets
 func getBucketItems(bucketName string) {
 
-}
-
-func (s3manager *S3_manager) createBucket(client *s3.S3, bucketName string) {
-	_, err := client.CreateBucket(&s3.CreateBucketInput{Bucket: aws.String(bucketName)})
-	if err != nil {
-		panic(err)
-	}
-	println("Bucket created: ", bucketName)
 }
