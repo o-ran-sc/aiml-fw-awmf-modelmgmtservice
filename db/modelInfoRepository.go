78cff1e60be52f9aed93e6ac3c90ad8c4e1e069a
subhash kumar singh <subh.singh@samsung.com>
2024-09-30 14:30:07 +0000

    Interface for data layer
    Interface for data layer.
    
    Change-Id: I4f9d57f346940c3f52240d9a91ffd12d50e9facf
    Signed-off-by: subhash kumar singh <subh.singh@samsung.com>

---
+++ db/modelInfoRepository.go
@@ -0,0 +1,53 @@
+/*
+==================================================================================
+Copyright (c) 2024 Samsung Electronics Co., Ltd. All Rights Reserved.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+	http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+==================================================================================
+*/
+package db
+
+import (
+	"gerrit.o-ran-sc.org/r/aiml-fw/awmf/modelmgmtservice/models"
+	"gorm.io/gorm"
+)
+
+type ModelInfoRepository struct {
+	db *gorm.DB
+}
+
+func NewModelInfoRepository(db *gorm.DB) *ModelInfoRepository {
+	return &ModelInfoRepository{
+		db: db,
+	}
+}
+
+func (repo *ModelInfoRepository) Create(modelInfo models.ModelInfo) error {
+	return nil
+}
+
+func (repo *ModelInfoRepository) GetByID(id string) (*models.ModelInfo, error) {
+	return nil, nil
+}
+
+func (repo *ModelInfoRepository) GetAll() []models.ModelInfo {
+	return nil
+}
+
+func (repo *ModelInfoRepository) Update(modelInfo models.ModelInfo) error {
+	return nil
+}
+
+func (repo *ModelInfoRepository) Delete(id string) error {
+	return nil
+}
--- go.mod
+++ go.mod
@@ -19,6 +19,8 @@ require (
 	github.com/go-playground/universal-translator v0.18.1 // indirect
 	github.com/go-playground/validator/v10 v10.14.0 // indirect
 	github.com/goccy/go-json v0.10.2 // indirect
+	github.com/jinzhu/inflection v1.0.0 // indirect
+	github.com/jinzhu/now v1.1.5 // indirect
 	github.com/jmespath/go-jmespath v0.4.0 // indirect
 	github.com/json-iterator/go v1.1.12 // indirect
 	github.com/klauspost/cpuid/v2 v2.2.4 // indirect
@@ -35,7 +37,8 @@ require (
 	golang.org/x/crypto v0.14.0 // indirect
 	golang.org/x/net v0.17.0 // indirect
 	golang.org/x/sys v0.13.0 // indirect
-	golang.org/x/text v0.13.0 // indirect
+	golang.org/x/text v0.14.0 // indirect
 	google.golang.org/protobuf v1.30.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
+	gorm.io/gorm v1.25.12 // indirect
 )
